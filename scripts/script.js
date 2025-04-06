401886442971916caf2df4b8f15986aad4cb395d
Riad45 <bingo45riad@gmail.com>
2025-04-06 16:02:32 +0600

    added feature (load videos by categories)

--- scripts/script.js
+++ scripts/script.js
@@ -1,71 +1,90 @@
 
-const loadCategories=()=>{
-    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-    .then(res=>res.json())
-    .then(data=>displayCategories(data.categories))
-    .catch(err=>console.log(`error: ${err}`))
+const loadCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then(res => res.json())
+    .then(data => displayCategories(data.categories))
+    .catch(err => console.log(`error: ${err}`))
+
+}
+
+const loadVideos = () => {
+
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then(res => res.json())
+    .then(data => displayVideos(data.videos))
+
+}
+
+const loadCategoryVideos = (id) => {
+
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then(res => res.json())
+    .then(data => displayVideos(data.category))
 
 }
 
 
+const displayCategories = (categories) => {
+  const categoryContainer = document.getElementById("category-container");
+  for (let cat of categories) {
+    const categoryDiv = document.createElement("div");
 
+    categoryDiv.innerHTML = `
+    <button onclick="loadCategoryVideos(${cat.category_id}) " class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>`
 
-const displayCategories=(categories)=>{
-    const categoryContainer = document.getElementById("category-container");
-    for(let cat of categories){
-        const categoryDiv = document.createElement("div");
-
-        categoryDiv.innerHTML=`<button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>`
-
-        categoryContainer.appendChild(categoryDiv);
-    }
+    categoryContainer.appendChild(categoryDiv);
+  }
 }
 
-const loadVideos = ()=>{
+const displayVideos = (videos) => {
 
-    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
-    .then(res=>res.json())
-    .then(data=>displayVideos(data.videos))
+  const videoContainer = document.getElementById("video-container");
 
-}
+  videoContainer.innerHTML = " "
 
-const displayVideos= (videos)=>{
+  videos.forEach(video => {
 
-    const videoContainer = document.getElementById("video-container");
+    const videoDiv = document.createElement("div");
+    videoDiv.innerHTML = `
+         <div class="bg-base-100 shadow-sm">
+          <figure>
+            <img
+              class="w-full h-[250px] object-cover rounded-md"
+              src="${video.thumbnail}"
+              alt="thumbnail"
+            />
+          </figure>
+          <div class="flex gap-5 px-0 py-5">
 
-    videos.forEach(video => {
+           <div class="avatar p-2">
+            <div class="w-16 rounded-full ">
+             <img src="${video.authors[0].profile_picture}" />
+            </div>
+           </div>
 
-        const videoDiv = document.createElement("div");
-        videoDiv.innerHTML= `
-        <div class="card bg-base-100  shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">${video.title}</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+
+
+            
+
+            <div class="info">
+              <h2 class="text-xl font-bold">
+                ${video.title}
+              </h2>
+              <p class="text-slate-400 text-sm">${video.authors[0].profile_name}</p>
+              <p class="text-slate-400 text-sm">${video.others.views}</p>
+            </div>
+          </div>
+        </div>
         `
 
-        videoContainer.appendChild(videoDiv);
-    
-        
-    });
+    videoContainer.appendChild(videoDiv);
+
+
+  });
 
 
 
 }
-
-
-
-
 
 
 loadCategories();
-loadVideos();
